ea6d5b1c3e10d18a578304ba8ea2c41a2066cacf
Firas Chaabani <firaschaabani1@gmail.com>
2019-10-13 22:05:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var kittens = ["Milo","Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten (kittens) {
-kittens.push("ralph");
+kittens.push("ralph")
 return kittens ()
 console.log (kittens)
 }
@@ -13,10 +13,12 @@ kittens.unshift("Bob")
 return kittens();
 }
 function destructivelyremovelastkittens(kittens){
-  return kittens.pop();
+ kittens.pop()
+ return kittens()
 }
-function destructivelyremovefirstkittens (kittens){
-  return kittens.shift()
+function destructivelyremovefirstkittens(kittens){
+   kittens.shift()
+   return kittens()
 }
 function  appendKitten (name){
   return kittens ()
@@ -25,7 +27,7 @@ function prependKitten(name) {
   return kittens ()
 }
 function removelastKitten() {
-return kittens.slice (-1)
+ kittens.push 
 }
 function removefirstKitten() {
   return kittens.splce(-1)
